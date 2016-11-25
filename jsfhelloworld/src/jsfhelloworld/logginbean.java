f12d63a09496ae5ac960328c4823e8abfc816c31
giorgio.gamberi <giorgio.gamberi@nbk-gamberi.ad.aruba.it>
2016-11-25 10:21:07 +0100

    puppa2

---
+++ jsfhelloworld/src/jsfhelloworld/logginbean.java
@@ -0,0 +1,29 @@
+package jsfhelloworld;
+
+public class logginbean {
+	 private String name;
+	    private String password;
+
+
+	    public String getName() {
+	        return name;
+	    }
+
+
+	    public void setName (final String name)
+	    {
+	        this.name = name;
+	    }
+
+
+	    public String getPassword ()
+	    {
+	        return password;
+	    }
+
+
+	    public void setPassword (final String password)
+	    {
+	        this.password = password;
+	    }
+}
